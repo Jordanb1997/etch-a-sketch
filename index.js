996976caea021b71441e599119bea01e87a02350
Jordan Bannister <40512499+Jordanb1997@users.noreply.github.com>
2023-05-03 15:17:24 +1000

    grid creation

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function generateGrid(gridSize) {
-    const gridContainer = document.getElementById('grid-container');
+const gridContainer = document.getElementById('grid-container');
+for (let i = 0; i < 16 * 16; i++) {
     let cell = document.createElement('div');
+    cell.className = ("grid-cell");
+    gridContainer.appendChild(cell);
+}
 
+/* function createGrid(gridSize) {
     for (let i = 0; i < gridSize * gridSize; i++) {
+        let cell = document.createElement('div');
         gridContainer.appendChild(cell);
         console.log("loop:" + 1)
     }
-}
+} */
